be6f12158087b744107600ad2cd606195f5eba36
Ben Nagy <ben.b.nagy@gsk.com>
2025-12-04 10:53:10 +0000

    feat: day-04 part2

--- day-04/src/lib.rs
+++ day-04/src/lib.rs
@@ -15,7 +15,30 @@ pub fn part1(input: &str) -> String {
 }
 
 pub fn part2(input: &str) -> String {
-    todo!("Implement part 2");
+    let mut grid: Grid = input.lines().map(|line| line.chars().collect()).collect();
+
+    let mut removed_rolls = 0;
+    loop {
+        let accessible_positions = get_accessible_positions(&grid);
+        removed_rolls += accessible_positions.len();
+        if accessible_positions.is_empty() {
+            break;
+        }
+        for (x, y) in accessible_positions {
+            grid[y][x] = '.';
+        }
+    }
+    removed_rolls.to_string()
+}
+
+fn get_accessible_positions(grid: &Grid) -> Vec<(usize, usize)> {
+    let accessible_positions: Vec<(usize, usize)> = iproduct!(0..grid[0].len(), 0..grid.len())
+        .filter(|(x, y)| grid[*y][*x] == '@')
+        .map(|(x, y)| (x as isize, y as isize))
+        .filter(|position| position_accessible(&grid, *position))
+        .map(|(x, y)| (x as usize, y as usize))
+        .collect();
+    accessible_positions
 }
 
 fn position_accessible(grid: &Grid, position: (isize, isize)) -> bool {
@@ -64,9 +87,19 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let input = r#""#;
+        let input = r#"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"#;
 
-        let expected = r#""#;
+        let expected = r#"43"#;
 
         let result = part2(input);
         assert_eq!(result, expected);
