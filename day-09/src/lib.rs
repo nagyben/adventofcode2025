c935ea4f3dd400f0a12ea4bf160c5ee813de6241
Ben Nagy <ben.b.nagy@gsk.com>
2025-12-09 18:50:24 +0000

    wip day-09 part2

--- day-09/src/lib.rs
+++ day-09/src/lib.rs
@@ -1,3 +1,4 @@
+use geo::{Coord, Intersects, LineString, Polygon, point};
 use itertools::Itertools;
 
 type Tile = (usize, usize);
@@ -28,11 +29,70 @@ pub fn part1(input: &str) -> String {
 }
 
 pub fn part2(input: &str) -> String {
-    todo!("Implement part 2");
+    let tiles = input
+        .lines()
+        .map(|line| {
+            let nums = line
+                .split(',')
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>();
+            (nums[0], nums[1])
+        })
+        .collect::<Vec<Tile>>();
+
+    let poly = Polygon::new(
+        LineString::from(
+            tiles
+                .iter()
+                .map(|(a, b)| Coord {
+                    x: *a as f64,
+                    y: *b as f64,
+                })
+                .collect::<Vec<_>>(),
+        ),
+        vec![],
+    );
+
+    let tile_pairs: Vec<_> = tiles
+        .into_iter()
+        .tuple_combinations()
+        .map(|(a, b)| (a, b, tile_area(a, b)))
+        .collect();
+
+    println!("Total tile pairs: {}", tile_pairs.len());
+
+    let valid_tile_pairs: Vec<_> = tile_pairs
+        .into_iter()
+        .filter(|(a, b, _)| {
+            let points = get_points_for_tile_pair(*a, *b);
+            points.iter().all(|p| poly.intersects(p))
+        })
+        .collect();
+
+    println!("Valid tile pairs: {}", valid_tile_pairs.len());
+
+    valid_tile_pairs
+        .into_iter()
+        .max_by(|a, b| a.2.cmp(&b.2))
+        .unwrap()
+        .2
+        .to_string()
 }
 
 fn tile_area(a: Tile, b: Tile) -> usize {
     (a.0.abs_diff(b.0) + 1) * (a.1.abs_diff(b.1) + 1)
+}
+
+fn get_points_for_tile_pair(a: Tile, b: Tile) -> Vec<geo::Point> {
+    let (x1, y1) = a;
+    let (x2, y2) = b;
+
+    vec![
+        point!(x:x1 as f64, y:y1 as f64),
+        point!(x:x1 as f64, y:y2 as f64),
+        point!(x:x2 as f64, y:y1 as f64),
+        point!(x:x2 as f64, y:y2 as f64),
+    ]
 }
 
 #[cfg(test)]
@@ -59,9 +119,17 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let input = r#""#;
+        let input = r#"7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3
+"#;
 
-        let expected = r#""#;
+        let expected = r#"24"#;
 
         let result = part2(input);
         assert_eq!(result, expected);
